248d6033e345d8b4825a7762c7663b583e327f62
nicognaW <nicognaW@users.noreply.github.com>
2024-11-01 10:19:21 +0000

    deploy: b5ca1dd0bbd859f06fe119e6ec21f731fa8208bc

---
+++ client/assets/dashboard.event-types-BWfFLnhk.js
@@ -0,0 +1,1 @@
+import{j as t}from"./jsx-runtime-DR3ia65l.js";import{O as r}from"./index-Cb6loArw.js";function s(){return t.jsx(r,{})}export{s as default};
---
+++ client/assets/skeleton-CpV1WJUG.js
@@ -0,0 +1,1 @@
+import{j as m}from"./jsx-runtime-DR3ia65l.js";import{c as o}from"./utils-jM7Hu9J5.js";function a({className:r,...e}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-primary/10",r),...e})}export{a as S};
