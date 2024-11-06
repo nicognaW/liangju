4f33532977293118ff06736cbd1bd4f93c82bd22
nicognaw <nicognaw@outlook.com>
2024-11-06 17:32:23 +0800

    Add validations to API URL setting

--- app/components/nav-settings.tsx
+++ app/components/nav-settings.tsx
@@ -11,12 +11,35 @@ import { Input } from "./ui/input"
 import { SettingsIcon } from "lucide-react"
 import { SidebarMenuButton } from "./ui/sidebar"
 import { useAPIStore } from "~/lib/api"
+import { z } from "zod"
+import { useRef, useState } from "react"
+import { cn } from "~/lib/utils"
+import { useSearchParams } from "@remix-run/react"
 
 export function Settings() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [valid, setValid] = useState(true);
   const addr = useAPIStore((state) => state.addr);
   const updateAddr = useAPIStore((state) => state.updateAddr);
+  const [open, setOpen] = useState((searchParams.get("settings") === "true"));
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  return <Dialog>
+  const tryChangeOpen = (open: boolean) => {
+    if (!open && !valid) {
+      setTimeout(() => inputRef?.current?.focus(), 0);
+      setOpen(true);
+      return;
+    }
+    setOpen(open);
+
+    setSearchParams((prev) => {
+      open ? prev.set("settings", "true") : prev.delete("settings");
+      return prev;
+    });
+  };
+
+  return <Dialog open={open} onOpenChange={tryChangeOpen}>
     {<SidebarMenuButton asChild size="sm">
       <DialogTrigger asChild>
         <button>
@@ -39,11 +62,34 @@ export function Settings() {
           </Label>
           <Input
             id="name"
+            type="text"
             defaultValue={addr}
-            onChange={(e) => {
-              updateAddr(e.target.value);
+            ref={inputRef}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                tryChangeOpen(false);
+              }
             }}
-            className="col-span-3"
+            onChange={(e) => {
+              const parsed = z
+                .string()
+                .url()
+                .transform((value) => {
+                  if (!value.startsWith("http://") && !value.startsWith("https://")) {
+                    e.target.value = `http://${value}`;
+                    return `http://${value}`;
+                  }
+                  return value;
+                })
+                .safeParse(e.target.value);
+              if (parsed.success) {
+                setValid(true);
+                updateAddr(e.target.value);
+              } else {
+                setValid(false);
+              }
+            }}
+            className={cn("col-span-3", (!valid) && "focus-visible:ring-destructive")}
           />
         </div>
       </div>
