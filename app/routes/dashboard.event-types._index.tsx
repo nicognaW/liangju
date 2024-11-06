a44fdf6121a163e1c283a80d026597759bb937c7
nicognaw <nicognaw@outlook.com>
2024-11-06 14:03:00 +0800

    Optimize empty data display

--- app/routes/dashboard.event-types._index.tsx
+++ app/routes/dashboard.event-types._index.tsx
@@ -31,44 +31,51 @@ export default function Page() {
           <Button type="submit">创建</Button>
         </Form>
       </div>
-      <ul className="border-subtle flex flex-col overflow-hidden rounded-md border divide-subtle !static w-full divide-y">
-        {data.map((item, index) =>
-          <li key={index}>
-            <div className="hover:bg-muted p-4 group flex w-full max-w-full items-center justify-between overflow-hidden px-4 py-4 sm:px-6">
-              <Link to={`/dashboard/event-types/${item.id}`} className="flex flex-col w-full">
+      {(data.length > 0) &&
+        <ul className="border-subtle flex flex-col overflow-hidden rounded-md border divide-subtle !static w-full divide-y">
+          {data.map((item, index) =>
+            <li key={index}>
+              <div className="hover:bg-muted p-4 group flex w-full max-w-full items-center justify-between overflow-hidden px-4 py-4 sm:px-6">
+                <Link to={`/dashboard/event-types/${item.id}`} className="flex flex-col w-full">
+                  <div>
+                    <span className="text-primary font-semibold mr-1">{item.title}</span>
+                    <small className="text-muted-foreground font-normal leading-4 sm:inline">{item.url}</small>
+                  </div>
+                  <span className="text-muted-foreground break-words py-1 text-sm font-normal line-clamp-4">{item.description}</span>
+                  <div className="mt-2 flex flex-wrap gap-x-2 gap-y-1">
+                    <Badge className="font-medium inline-flex items-center justify-center rounded gap-x-1 py-1 px-1.5 text-xs leading-3 bg-sidebar-accent text-sidebar-accent-foreground" variant="secondary">
+                      <Clock className="w-3 h-3" />
+                      <span>
+                        {item.duration}m
+                      </span>
+                    </Badge>
+                  </div>
+                </Link>
                 <div>
-                  <span className="text-primary font-semibold mr-1">{item.title}</span>
-                  <small className="text-muted-foreground font-normal leading-4 sm:inline">{item.url}</small>
-                </div>
-                <span className="text-muted-foreground break-words py-1 text-sm font-normal line-clamp-4">{item.description}</span>
-                <div className="mt-2 flex flex-wrap gap-x-2 gap-y-1">
-                  <Badge className="font-medium inline-flex items-center justify-center rounded gap-x-1 py-1 px-1.5 text-xs leading-3 bg-sidebar-accent text-sidebar-accent-foreground" variant="secondary">
-                    <Clock className="w-3 h-3" />
-                    <span>
-                      {item.duration}m
-                    </span>
-                  </Badge>
-                </div>
-              </Link>
-              <div>
-                <div className="flex [&>*:first-child]:ml-0 [&>*:first-child]:rounded-l-md [&>*:first-child]:border-l [&>*:last-child]:rounded-r-md [&>a]:-ml-[1px] hover:[&>a]:z-[1] [&>button]:-ml-[1px] hover:[&>button]:z-[1] [&_a]:rounded-none [&_button]:rounded-none">
-                  <Link className="rounded-l-md whitespace-nowrap items-center text-sm font-medium relative rounded-md transition disabled:cursor-not-allowed flex justify-center text-accent-foreground border border-default bg-white hover:bg-muted disabled:border-subtle disabled:bg-opacity-30 disabled:text-muted disabled:hover:bg-opacity-30 disabled:hover:text-muted disabled:hover:border-subtle disabled:hover:bg-white h-9 px-4 py-2.5 min-h-[36px] min-w-[36px] !p-2 hover:border-default" to={item.url}>
-                    <ExternalLink className="w-4 h-4" />
-                  </Link>
-                  <button disabled className="disabled:text-neutral-400 disabled:hover:text-neutral-400 whitespace-nowrap items-center text-sm font-medium relative rounded-md transition disabled:cursor-not-allowed flex justify-center text-accent-foreground border border-default bg-white hover:bg-muted disabled:border-subtle disabled:bg-opacity-30 disabled:text-muted disabled:hover:bg-opacity-30 disabled:hover:text-muted disabled:hover:border-subtle disabled:hover:bg-white h-9 px-4 py-2.5 min-h-[36px] min-w-[36px] !p-2 hover:border-default">
-                    <LinkIcon className="w-4 h-4" />
-                  </button>
-                  <Form action={`${item.id}/delete`} method="post" navigate={false}>
-                    <button className="disabled:text-neutral-400 disabled:hover:text-neutral-400 whitespace-nowrap items-center text-sm font-medium relative rounded-md transition disabled:cursor-not-allowed flex justify-center text-accent-foreground border border-default bg-white hover:bg-destructive hover:text-destructive-foreground disabled:border-subtle disabled:bg-opacity-30 disabled:text-muted disabled:hover:bg-opacity-30 disabled:hover:text-muted disabled:hover:border-subtle disabled:hover:bg-white h-9 px-4 py-2.5 min-h-[36px] min-w-[36px] !p-2 hover:border-default">
-                      <Delete className="w-4 h-4" />
+                  <div className="flex [&>*:first-child]:ml-0 [&>*:first-child]:rounded-l-md [&>*:first-child]:border-l [&>*:last-child]:rounded-r-md [&>a]:-ml-[1px] hover:[&>a]:z-[1] [&>button]:-ml-[1px] hover:[&>button]:z-[1] [&_a]:rounded-none [&_button]:rounded-none">
+                    <Link className="rounded-l-md whitespace-nowrap items-center text-sm font-medium relative rounded-md transition disabled:cursor-not-allowed flex justify-center text-accent-foreground border border-default bg-white hover:bg-muted disabled:border-subtle disabled:bg-opacity-30 disabled:text-muted disabled:hover:bg-opacity-30 disabled:hover:text-muted disabled:hover:border-subtle disabled:hover:bg-white h-9 px-4 py-2.5 min-h-[36px] min-w-[36px] !p-2 hover:border-default" to={item.url}>
+                      <ExternalLink className="w-4 h-4" />
+                    </Link>
+                    <button disabled className="disabled:text-neutral-400 disabled:hover:text-neutral-400 whitespace-nowrap items-center text-sm font-medium relative rounded-md transition disabled:cursor-not-allowed flex justify-center text-accent-foreground border border-default bg-white hover:bg-muted disabled:border-subtle disabled:bg-opacity-30 disabled:text-muted disabled:hover:bg-opacity-30 disabled:hover:text-muted disabled:hover:border-subtle disabled:hover:bg-white h-9 px-4 py-2.5 min-h-[36px] min-w-[36px] !p-2 hover:border-default">
+                      <LinkIcon className="w-4 h-4" />
                     </button>
-                  </Form>
+                    <Form action={`${item.id}/delete`} method="post" navigate={false}>
+                      <button className="disabled:text-neutral-400 disabled:hover:text-neutral-400 whitespace-nowrap items-center text-sm font-medium relative rounded-md transition disabled:cursor-not-allowed flex justify-center text-accent-foreground border border-default bg-white hover:bg-destructive hover:text-destructive-foreground disabled:border-subtle disabled:bg-opacity-30 disabled:text-muted disabled:hover:bg-opacity-30 disabled:hover:text-muted disabled:hover:border-subtle disabled:hover:bg-white h-9 px-4 py-2.5 min-h-[36px] min-w-[36px] !p-2 hover:border-default">
+                        <Delete className="w-4 h-4" />
+                      </button>
+                    </Form>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-        )}
-      </ul>
+            </li>
+          )}
+        </ul>
+      }
+      {data.length === 0 && <div className="flex !static w-full justify-center !mt-12">
+        <h2 className="text-2xl text-muted-foreground">
+          空
+        </h2>
+      </div>}
     </div>
   );
 }
